21d2e887a433c30a28501421e2b65e7650a5832a
Dávid Bedők <david@bedok.hu>
2015-05-23 14:03:34 +0200

    ADD: RSS Reader project

---
+++ RSSReader/RSSReader/RSSProcessor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RSSReaderSpace
+{
+
+    public delegate void RelevantArticle(String source, String article);
+    public delegate void RSSProcessorError(String source, String message);
+
+    public class RSSProcessor
+    {
+        private static bool OFFLINE_MODE = true;
+        private static String CHILD_ELEMENT = "item";
+        private static String TITLE_ELEMENT = "title";
+
+        private readonly String source;
+        private readonly List<String> keywords;
+        private readonly String url;
+
+        public event RelevantArticle articleHandler;
+        public event RSSProcessorError errorHandler;
+
+        public RSSProcessor(String source, String url, List<String> keywords)
+        {
+            this.source = source;
+            this.keywords = keywords;
+            this.url = url;
+        }
+
+        public void run()
+        {
+            try
+            {
+                this.process(XDocument.Load( OFFLINE_MODE ? source : url ));
+            }
+            catch (Exception e)
+            {
+                if (this.errorHandler != null)
+                {
+                    this.errorHandler(source, e.Message);
+                }
+            }       
+        }
+
+        private void process(XDocument xdoc)
+        {
+            List<XElement> children = xdoc.Root.Descendants(CHILD_ELEMENT).ToList();
+            foreach (string keyword in this.keywords)
+            {
+                List<String> relevantTitles = children.Where(x => x.Element(TITLE_ELEMENT).Value.Contains(keyword)).Select(x => x.Element(TITLE_ELEMENT).Value).ToList();
+                if (this.articleHandler != null)
+                {
+                    foreach (String article in relevantTitles)
+                    {
+                        this.articleHandler(this.source, article);
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ RSSReader/RSSReader/ReaderWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace RSSReaderSpace
+{
+  
+    public partial class ReaderWindow : Window
+    {
+       
+        private static char KEYWORD_SEPARATOR = ',';
+
+        public ReaderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Go_Click(object sender, RoutedEventArgs e)
+        {
+            List<String> keywords = this.getKeywords();
+            this.startNewRssProcessorThreadString("index.xml", "http://users.nik.uni-obuda.hu/cseri/hp/index.xml", keywords);
+            this.startNewRssProcessorThreadString("origo.xml", "http://users.nik.uni-obuda.hu/cseri/hp/origo.xml", keywords);
+        }
+
+        private List<String> getKeywords()
+        {
+            return this.keywordsTextBox.Text.Split(KEYWORD_SEPARATOR).ToList();
+        }
+
+        private void startNewRssProcessorThreadString( String source, String rssUrl, List<String> keywords)
+        {
+            RSSProcessor processor = new RSSProcessor(source, rssUrl, keywords);
+            processor.articleHandler += rssProcessorFinishEventHandler;
+            processor.errorHandler += rssProcessorErrorEventHandler;
+            Thread thread = new Thread(processor.run);
+            thread.Start();
+        }
+
+        private void rssProcessorErrorEventHandler(String source, String message)
+        {
+            Dispatcher.Invoke(() => MessageBox.Show("Error: " + message));
+        }
+
+        private void rssProcessorFinishEventHandler(String fileCache, String article)
+        {
+            Dispatcher.Invoke(() => this.resultListView.Items.Add(article));
+        }
+
+    }
+}
